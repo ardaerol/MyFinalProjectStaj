9e872da2f767098a11229cfc48fc7f4030fb2d8b
Aziz Arda Erol <20190311036@dogus.edu.tr>
2022-03-01 10:48:06 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductTest();
+            //CategoryTest();
+            Product product1 = new Product { ProductId = 78, CategoryId = 1, ProductName = "arda ürün", UnitPrice = 12, UnitsInStock = 1 };
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            productManager.Add(product1);
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/"+product.CategoryName);
+
+            }
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //Generic constraint
+    //class : T referans tip olabilir demek "where T:class"
+    //,IEntity : IEntity olabilir veya ondan implemente edilmiş bir nesne olabilir
+    //new() newlenebilir olmalı IEntity newlenemez yani T entitiesteki nesneleri referans alabilir
+  public  interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        
+    }
+}
